c99d6376e699477c4ba00265553813716114921c
Fisher.Xiong <541184872@qq.com>
2019-07-02 14:15:34 +0800

    Update common.js

--- common.js
+++ common.js
@@ -1,3 +1,18 @@
+
+/**
+ * 数字分割
+ * @param num
+ */
+toThousands(num) {
+   let num = (num || 0).toString(), result = '';
+   while (num.length > 3) {
+       result = ',' + num.slice(-3) + result;
+       num = num.slice(0, num.length - 3);
+   }
+   if (num) { result = num + result; }
+   return result;
+ }
+
 /**
  * 两位小数金额输入
  * @param target
